ba79a23e51e5571d5ea8c8e416760ba274b444c9
Dannia <danniarivera035@gmail.com>
2024-05-10 00:10:20 -0500

    loads results from a file

--- data_manager.py
+++ data_manager.py
@@ -5,4 +5,10 @@ class DataManager:
 
     def save_results(self,candidates: list):
         '''Saves the candidate results to the file'''
-        
+        with open(self.filename,'w') as file:
+            for candidate in candidates:
+                file.write(f'{candidate.name}, {candidate.votes}\n')
+
+    def load_results(self) -> list:
+        '''Loads the candidate results from the file'''
+        candidates = []
