c39045c7c9ea4051e4c0da67ca841753e1f2f77b
Dannia <danniarivera035@gmail.com>
2024-05-09 22:37:57 -0500

    counts the votes after every click

--- vote_counter.py
+++ vote_counter.py
@@ -1,3 +1,8 @@
 class Candidate:
     def __init__(self, name: str):
+        '''Initialize Candidate object with a name and votes count.'''
+        self.name = name
+        self.votes = 0
+
+    def vote(self):
         
