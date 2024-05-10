6533aefb66d03c10ece3746e7bb317b957eedee7
Dannia <danniarivera035@gmail.com>
2024-05-10 00:34:38 -0500

    making logic for the buttons

--- voting_app.py
+++ voting_app.py
@@ -8,3 +8,26 @@ def back_to_main_menu(root):
     root.destroy()
     root.main_menu.deiconify()
     root.main_menu.lift()
+
+class VotingApp(tk.Tk):
+    def __init__(self, input_mode=True, candidate_names=None, main_menu=None):
+        super().__init__()
+        self.title("Voting System")
+        self.geometry("400x300")
+        self.configure(bg="#F0F0F0")
+
+        self.input_mode = input_mode
+        self.vote_system = VoteSystem()
+        self.previous_votes = []
+        self.candidate_names = candidate_names if candidate_names is not None else []
+        self.main_menu = main_menu  # Store a reference to the main menu window
+
+        self.main_frame = tk.Frame(self, bg="#F0F0F0")
+        self.main_frame.pack(expand=True, fill=tk.BOTH)
+
+        if self.input_mode:
+            self.create_input_screen()
+        else:
+            self.generate_candidates()
+
+        
