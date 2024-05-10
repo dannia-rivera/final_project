cc076b53fca15424fbb787f61e9a4ca48a028704
Dannia <danniarivera035@gmail.com>
2024-05-10 00:07:01 -0500

    saves the results to the file

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,8 @@
 class DataManager:
     def __init__(self, filename: str):
         '''Initializes the data manager with filename'''
+        self.filename = filename
+
+    def save_results(self,candidates: list):
+        '''Saves the candidate results to the file'''
+        
