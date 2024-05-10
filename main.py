de99435c9c3743d8c2cee2acd36a528dcbc38afa
Dannia <danniarivera035@gmail.com>
2024-05-10 00:19:47 -0500

    creating the input screen

--- main.py
+++ main.py
@@ -17,3 +17,22 @@ class MainScreen(tk.Tk):
         self.create_widgets()
 
     def set_main_menu(self, main_menu):
+        self.main_menu = main_menu
+
+    def create_widgets(self):
+        label = tk.Lbael(self.main_frame, text="Welcome to the Voting System!", font=("Helvetica", 16))
+        label.pack(pady=20)
+
+        options_frame = tk.Frame(self.main_frame, bg="#F0F0F0")
+        options_frame.pack()
+
+        input_button = tk.Button(options_frame, text="Input Names", command=self.input_names, bg="#64B5F6", fg="white",
+                                 font=("Helvetica", 12), relief=tk.FLAT)
+        input_button.grid(row=0, column=0, padx=20, pady=10)
+
+        random_button = tk.Button(options_frame, text="Random Names", command=self.random_names, bg="#64B5F6",
+                                  fg="white", font=("Helvetica", 12), relief=tk.FLAT)
+        random_button.grid(row=0, column=1, padx=20, pady=10)
+
+    def input_names(self):
+        '''Hide the main menu window and open the input screen'''
